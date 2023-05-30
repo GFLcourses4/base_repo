9b85ad9f242c2088990d50bad92a5d877d889f43
akedyk <artem.kedyk@geeksforless.com>
2023-05-30 10:48:19 +0300

    - Init application. - Test message. - Test second message.

---
+++ src/main/java/com/example/base_repo/BaseRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.base_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseRepoApplication.class, args);
+    }
+
+}
