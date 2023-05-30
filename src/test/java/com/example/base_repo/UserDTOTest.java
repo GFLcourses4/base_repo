cc9bbdea4b27122985051f9d3631c0e57a540564
akedyk <artem.kedyk@geeksforless.com>
2023-05-30 11:02:13 +0300

    - Create UserDTO. - Create Test for UserDTO.

---
+++ src/test/java/com/example/base_repo/UserDTOTest.java
@@ -0,0 +1,20 @@
+package com.example.base_repo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDTOTest {
+
+    @Test
+    public void testEquals() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        userDTO.setName("Test");
+        UserDTO userConstructorDTO = new UserDTO(1, "Test");
+
+        Assertions.assertEquals(userDTO, userConstructorDTO);
+    }
+
+}
